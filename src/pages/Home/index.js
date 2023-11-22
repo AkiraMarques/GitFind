a4114fec44bc4c47c6fa0aa602a38d1d2ffa4630
AkiraMarques <marqueslucas504@gmail.com>
2023-11-21 22:05:47 -0300

    adicionado perfil de exemplo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,12 +8,21 @@ function App() {
     <div className="App">
       <Header />
       <div className='conteudo'>
-        <img src={background} className='background' alt='background app'/>
+        <img src={background} className='background' alt='background app' />
         <div className='info'>
           <div>
             <input name='usuario' placeholder='@username' />
             <button>Buscas</button>
           </div>
+          <div className='perfil'>
+            <img src='https://avatars.githubusercontent.com/u/146784142?s=400&u=be922ba331c0a900721f48362b0c29a2b96d6e52&v=4' className='profile' alt='imagem de perfil'/>
+            <div>
+              <h3>Lucas Marques</h3>
+              <span>@AkiraMarques</span>
+              <p>Descrição</p>
+            </div>
+          </div>
+          <hr />
         </div>
       </div>
     </div>
